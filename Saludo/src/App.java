496455c1764d92927bfe6201dfc2be473ec5bf21
AlfonsoMF-UTIM <96090069+AlfonsoMF-UTIM@users.noreply.github.com>
2022-05-09 17:12:08 -0500

    Inicio
    El primer programa con java

---
+++ Saludo/src/App.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Saludos desde java", "Inciio", 1);
+
+    }
+}
